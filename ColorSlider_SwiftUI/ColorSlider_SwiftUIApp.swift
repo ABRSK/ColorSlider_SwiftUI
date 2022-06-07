897bead64574c13da138b02154f3c6f6c4552f89
AreyBRSK <areybrsk@gmail.com>
2022-06-07 12:54:56 +0300

    Initial Commit

---
+++ ColorSlider_SwiftUI/ColorSlider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlider_SwiftUIApp.swift
+//  ColorSlider_SwiftUI
+//
+//  Created by Андрей Барсук on 07.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
